4102368785d048eb4dc1603f79f398226dd65ccd
Rishabh <rishabh.dugar.com@gmail.com>
2018-04-14 18:54:02 +0530

    working with key in logs

---
+++ app/src/main/java/hashtags/autohash/MainActivity.java
@@ -0,0 +1,411 @@
+package hashtags.autohash;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Credentials;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Authenticator;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        selectImage();
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                /*PostImageToImaggaAsync postImageToImaggaAsync = new PostImageToImaggaAsync();
+                postImageToImaggaAsync.execute();*/
+                GetTagFromImaggaAsync getTagFromImaggaAsyncobj = new GetTagFromImaggaAsync();
+                getTagFromImaggaAsyncobj.execute();
+            }
+        });
+    }
+    public class PostImageToImaggaAsync extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String response = postImageToImagga(picturePath);
+                Log.i("imagga", response);
+                JSONObject jsonObject = new JSONObject(response);
+                Log.i("imagga id", jsonObject.getString("id"));
+                //returnid=jsonObject.getString("id");
+                //int age = jsonObject.getInt("age");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            GetTagFromImaggaAsync getTagFromImaggaAsyncobj = new GetTagFromImaggaAsync();
+            getTagFromImaggaAsyncobj.execute();
+        }
+    }
+    public class GetTagFromImaggaAsync extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String response = getTagFromImagga("f3eab0f0d71283e46e69934f3d0a50ef");
+                Log.i("imagga tag", response);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+        }
+    }
+    public String postImageToImagga(String filepath) throws Exception {
+        HttpURLConnection connection = null;
+        DataOutputStream outputStream = null;
+        InputStream inputStream = null;
+
+        String twoHyphens = "--";
+        String boundary =  "*****"+Long.toString(System.currentTimeMillis())+"*****";
+        String lineEnd = "\r\n";
+
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1*1024*1024;
+
+        String filefield = "image";
+
+        String[] q = filepath.split("/");
+        int idx = q.length - 1;
+
+        File file = new File(filepath);
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        URL url = new URL("https://api.imagga.com/v1/content");
+        connection = (HttpURLConnection) url.openConnection();
+
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+        connection.setUseCaches(false);
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Connection", "Keep-Alive");
+        connection.setRequestProperty("User-Agent", "Android Multipart HTTP Client 1.0");
+        connection.setRequestProperty("Content-Type", "multipart/form-data; boundary="+boundary);
+        connection.setRequestProperty("Authorization", "Basic YWNjX2RiZTI4OGQ3ZTlkOTQ5YjphYWJjNDE2MmI3YjdjYWFhNDcwOTc1Y2E0ZDJkZTdkNg==");
+
+        outputStream = new DataOutputStream(connection.getOutputStream());
+        outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+        outputStream.writeBytes("Content-Disposition: form-data; name=\"" + filefield + "\"; filename=\"" + q[idx] +"\"" + lineEnd);
+        outputStream.writeBytes("Content-Type: image/jpeg" + lineEnd);
+        outputStream.writeBytes("Content-Transfer-Encoding: binary" + lineEnd);
+        outputStream.writeBytes(lineEnd);
+
+        bytesAvailable = fileInputStream.available();
+        bufferSize = Math.min(bytesAvailable, maxBufferSize);
+        buffer = new byte[bufferSize];
+
+        bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+        while(bytesRead > 0) {
+            outputStream.write(buffer, 0, bufferSize);
+            bytesAvailable = fileInputStream.available();
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+        }
+
+        outputStream.writeBytes(lineEnd);
+        outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+        inputStream = connection.getInputStream();
+
+        int status = connection.getResponseCode();
+        if (status == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+
+            inputStream.close();
+            connection.disconnect();
+            fileInputStream.close();
+            outputStream.flush();
+            outputStream.close();
+
+            return response.toString();
+        } else {
+            throw new Exception("Non ok response returned");
+        }
+    }
+    public String getTagFromImagga(String id) throws Exception {
+
+ String basicAuth = "YWNjX2RiZTI4OGQ3ZTlkOTQ5YjphYWJjNDE2MmI3YjdjYWFhNDcwOTc1Y2E0ZDJkZTdkNg==";//Base64.getEncoder().encodeToString(credentialsToEncode.getBytes(StandardCharsets.UTF_8));
+
+        String endpoint_url = "https://api.imagga.com/v1/tagging";
+        String image_url = id;
+
+        String url = endpoint_url + "?content=" + image_url;
+        URL urlObject = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) urlObject.openConnection();
+
+        connection.setRequestProperty("Authorization", "Basic " + basicAuth);
+
+        int responseCode = connection.getResponseCode();
+
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader connectionInput = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        String jsonResponse = connectionInput.readLine();
+
+        connectionInput.close();
+
+        System.out.println(jsonResponse);
+        return jsonResponse;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void selectImage() {
+
+
+
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        builder.setTitle("Add Photo!");
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+
+            @Override
+
+            public void onClick(DialogInterface dialog, int item) {
+
+                if (options[item].equals("Take Photo"))
+
+                {
+
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+
+                    startActivityForResult(intent, 1);
+
+                }
+
+                else if (options[item].equals("Choose from Gallery"))
+
+                {
+
+                    Intent intent = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                    startActivityForResult(intent, 2);
+
+
+
+                }
+
+                else if (options[item].equals("Cancel")) {
+
+                    dialog.dismiss();
+
+                }
+
+            }
+
+        });
+
+        builder.show();
+
+    }
+
+
+
+    @Override
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == 1) {
+
+                File f = new File(Environment.getExternalStorageDirectory().toString());
+
+                for (File temp : f.listFiles()) {
+
+                    if (temp.getName().equals("temp.jpg")) {
+
+                        f = temp;
+
+                        break;
+
+                    }
+
+                }
+
+                try {
+
+                    Bitmap bitmap;
+
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+
+
+                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),
+
+                            bitmapOptions);
+
+
+                    //viewImage.setImageBitmap(bitmap);
+
+
+                    String path = android.os.Environment
+
+                            .getExternalStorageDirectory()
+
+                            + File.separator
+
+                            + "Phoenix" + File.separator + "default";
+
+                    f.delete();
+
+                    OutputStream outFile = null;
+
+                    File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+
+                    try {
+
+                        outFile = new FileOutputStream(file);
+
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+
+                        outFile.flush();
+
+                        outFile.close();
+
+                    } catch (FileNotFoundException e) {
+
+                        e.printStackTrace();
+
+                    } catch (IOException e) {
+
+                        e.printStackTrace();
+
+                    } catch (Exception e) {
+
+                        e.printStackTrace();
+
+                    }
+
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+
+                }
+
+            } else if (requestCode == 2) {
+
+
+                Uri selectedImage = data.getData();
+
+                String[] filePath = {MediaStore.Images.Media.DATA};
+
+                Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+
+                c.moveToFirst();
+
+                int columnIndex = c.getColumnIndex(filePath[0]);
+
+               picturePath =  c.getString(columnIndex);
+
+                c.close();
+
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+
+                Log.w("path of image from ", picturePath + "");
+
+                //viewImage.setImageBitmap(thumbnail);
+
+            }
+
+        }
+    }
+    String picturePath = null;
+}
