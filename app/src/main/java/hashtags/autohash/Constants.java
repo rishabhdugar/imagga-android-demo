69806ac44466e36f413c643912353ca2af97bb7c
jiteshdugar <jitesh.com@gmail.com>
2018-04-15 12:54:56 +0530

    UI changes

--- app/src/main/java/hashtags/autohash/Constants.java
+++ app/src/main/java/hashtags/autohash/Constants.java
@@ -1,5 +1,5 @@
 package hashtags.autohash;
 
 public class Constants {
-    public static String basic_auth="<enter your basic auth here>";
+    public static String basic_auth="Basic YWNjX2RiZTI4OGQ3ZTlkOTQ5YjphYWJjNDE2MmI3YjdjYWFhNDcwOTc1Y2E0ZDJkZTdkNg==";
 }
